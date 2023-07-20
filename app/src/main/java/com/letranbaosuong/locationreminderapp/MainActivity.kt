9611a1461858765e92592b8430156410f73ebc27
Le Tran Bao Suong <letranbaosuong@gmail.com>
2023-07-20 09:24:47 +0700

    init project

---
+++ app/src/main/java/com/letranbaosuong/locationreminderapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.letranbaosuong.locationreminderapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
